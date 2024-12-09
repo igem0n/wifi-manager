8e2a15e5b076394619fcc101dec1caa4536777c2
Oleksii Martynovskyi <o.martynovskyi@gmail.com>
2024-12-09 16:00:47 +0100

    [FIX] Ensure wifi object is created and ran in thread

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from  wifi_manager import WifiManager
 import configparser, os
 from flask import Flask, request, jsonify, abort, json, render_template
 
-app = Flask(__name__)
 default_config = {
     "DEFAULT": {
         "interface": "wlan0",
@@ -22,62 +21,66 @@ def readConfig():
     port = parser["DEFAULT"]["port"]
     return {"interface" : interface, "gateway" : gateway, "port" : port}
 
+def create_app():
+    app = Flask(__name__)
+    app_config = readConfig()
+    wifi = WifiManager(app_config["interface"], app_config["gateway"])
 
-wifi : WifiManager
+    @app.teardown_appcontext
+    def cleanup(exception=None):
+        """Stop the wifi thread when the app shuts down."""
+        if wifi:
+            wifi.stop()
 
-app_config = readConfig()
-wifi = WifiManager(app_config["interface"], app_config["gateway"])
-wifi.start()
+
+    @app.get("/wifi/available")
+    def get_wifi_networks():
+        """Return the cached Wi-Fi networks and signal levels."""
+        return jsonify({"networks" : wifi.wifi_networks})
+
+    @app.post("/wifi/rescan")
+    def rescan_wifi_networks():
+        """Forces scan of Wi-Fi networks and signal levels."""
+        return jsonify({"networks" : wifi.rescan_networks()})
+
+    @app.get("/wifi/status")
+    def get_wifi_status():
+        """Return the cached Wi-Fi active connection status."""
+        return jsonify({
+            "active" : wifi.active_connections,
+            "hotspot" : wifi.hotspot_mode
+        })
+
+    @app.post("/wifi/connect")
+    def post_connect_to_wifi():
+        data = request.get_json()
+        ssid = data.get('ssid')
+        password = data.get('password')
+        #if connect_to_wifi_netowrk(ssid, password):
+        if wifi.connect_to_wifi(ssid, password):
+            return jsonify({"result" : "ok"})
+        else:
+            abort(400)
+
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        """Return JSON instead of HTML for HTTP errors."""
+        response = e.get_response()
+        # replace the body with JSON
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
+
+    @app.get('/')
+    def ssid_login():
+        return render_template('index.html')
+
+    wifi.start()
+
+    return app
     
-@app.teardown_appcontext
-def cleanup(exception=None):
-    """Stop the wifi thread when the app shuts down."""
-    if wifi:
-        wifi.stop()
-
-
-@app.get("/wifi/available")
-def get_wifi_networks():
-    """Return the cached Wi-Fi networks and signal levels."""
-    return jsonify({"networks" : wifi.wifi_networks})
-
-@app.post("/wifi/rescan")
-def rescan_wifi_networks():
-    """Forces scan of Wi-Fi networks and signal levels."""
-    return jsonify({"networks" : wifi.rescan_networks()})
-
-@app.get("/wifi/status")
-def get_wifi_status():
-    """Return the cached Wi-Fi active connection status."""
-    return jsonify({
-        "active" : wifi.active_connections,
-        "hotspot" : wifi.hotspot_mode
-    })
-
-@app.post("/wifi/connect")
-def post_connect_to_wifi():
-    data = request.get_json()
-    ssid = data.get('ssid')
-    password = data.get('password')
-    #if connect_to_wifi_netowrk(ssid, password):
-    if wifi.connect_to_wifi(ssid, password):
-        return jsonify({"result" : "ok"})
-    else:
-        abort(400)
-
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
-
-@app.get('/')
-def ssid_login():
-    return render_template('index.html')
+app = create_app()
